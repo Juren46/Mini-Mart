930d74d30f7a6220a95bfbbbad208453c06f3196
elio_0406 <stsupermarik@gmail.com>
2023-11-19 21:52:42 +0700

    update

--- GUI/PhanQuyenForm.Designer.cs
+++ GUI/PhanQuyenForm.Designer.cs
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             dgvPhanQuyen = new Guna.UI2.WinForms.Guna2DataGridView();
             Column2 = new DataGridViewTextBoxColumn();
             Column1 = new DataGridViewTextBoxColumn();
@@ -43,10 +43,10 @@
             Column5 = new DataGridViewImageColumn();
             Column6 = new DataGridViewImageColumn();
             panel1 = new Panel();
-            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
-            panel2 = new Panel();
             iconButton2 = new FontAwesome.Sharp.IconButton();
             iconButton1 = new FontAwesome.Sharp.IconButton();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)dgvPhanQuyen).BeginInit();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
@@ -55,42 +55,42 @@
             // dgvPhanQuyen
             // 
             dgvPhanQuyen.AllowUserToAddRows = false;
-            dataGridViewCellStyle6.BackColor = Color.White;
-            dgvPhanQuyen.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle1.BackColor = Color.White;
+            dgvPhanQuyen.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             dgvPhanQuyen.BackgroundColor = Color.FromArgb(242, 245, 250);
-            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle7.BackColor = Color.LightBlue;
-            dataGridViewCellStyle7.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle7.ForeColor = Color.FromArgb(30, 110, 142);
-            dataGridViewCellStyle7.SelectionBackColor = Color.LightBlue;
-            dataGridViewCellStyle7.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
-            dgvPhanQuyen.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = Color.LightBlue;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(30, 110, 142);
+            dataGridViewCellStyle2.SelectionBackColor = Color.LightBlue;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvPhanQuyen.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dgvPhanQuyen.ColumnHeadersHeight = 40;
             dgvPhanQuyen.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
             dgvPhanQuyen.Columns.AddRange(new DataGridViewColumn[] { Column2, Column1, Column3, Column4, Column5, Column6 });
-            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle9.BackColor = Color.White;
-            dataGridViewCellStyle9.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle9.ForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle9.SelectionBackColor = Color.FromArgb(227, 248, 251);
-            dataGridViewCellStyle9.SelectionForeColor = Color.FromArgb(71, 69, 94);
-            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.False;
-            dgvPhanQuyen.DefaultCellStyle = dataGridViewCellStyle9;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.White;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(227, 248, 251);
+            dataGridViewCellStyle4.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.False;
+            dgvPhanQuyen.DefaultCellStyle = dataGridViewCellStyle4;
             dgvPhanQuyen.Dock = DockStyle.Fill;
             dgvPhanQuyen.GridColor = Color.FromArgb(242, 245, 250);
             dgvPhanQuyen.Location = new Point(0, 0);
-            dgvPhanQuyen.Margin = new Padding(5, 5, 5, 5);
+            dgvPhanQuyen.Margin = new Padding(5);
             dgvPhanQuyen.Name = "dgvPhanQuyen";
             dgvPhanQuyen.ReadOnly = true;
-            dataGridViewCellStyle10.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle10.BackColor = Color.White;
-            dataGridViewCellStyle10.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            dataGridViewCellStyle10.ForeColor = Color.FromArgb(242, 245, 250);
-            dataGridViewCellStyle10.SelectionBackColor = Color.White;
-            dataGridViewCellStyle10.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle10.WrapMode = DataGridViewTriState.True;
-            dgvPhanQuyen.RowHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = Color.White;
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = Color.FromArgb(242, 245, 250);
+            dataGridViewCellStyle5.SelectionBackColor = Color.White;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            dgvPhanQuyen.RowHeadersDefaultCellStyle = dataGridViewCellStyle5;
             dgvPhanQuyen.RowHeadersVisible = false;
             dgvPhanQuyen.RowHeadersWidth = 51;
             dgvPhanQuyen.RowTemplate.DividerHeight = 3;
@@ -118,14 +118,14 @@
             dgvPhanQuyen.ThemeStyle.RowsStyle.Height = 50;
             dgvPhanQuyen.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(0, 80, 112);
             dgvPhanQuyen.ThemeStyle.RowsStyle.SelectionForeColor = Color.White;
-            dgvPhanQuyen.CellFormatting += sanPhamDataGridView_CellFormatting;
+            dgvPhanQuyen.CellFormatting += phanQuyenDataGridView_CellFormatting;
             dgvPhanQuyen.CellPainting += sanPhamDataGridView_CellPainting;
             // 
             // Column2
             // 
             Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            Column2.DefaultCellStyle = dataGridViewCellStyle8;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            Column2.DefaultCellStyle = dataGridViewCellStyle3;
             Column2.HeaderText = "#";
             Column2.MinimumWidth = 6;
             Column2.Name = "Column2";
@@ -197,36 +197,6 @@
             panel1.Size = new Size(1183, 62);
             panel1.TabIndex = 64;
             // 
-            // guna2TextBox1
-            // 
-            guna2TextBox1.CustomizableEdges = customizableEdges3;
-            guna2TextBox1.DefaultText = "";
-            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            guna2TextBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(0, 192, 192);
-            guna2TextBox1.Location = new Point(12, 12);
-            guna2TextBox1.Name = "guna2TextBox1";
-            guna2TextBox1.PasswordChar = '\0';
-            guna2TextBox1.PlaceholderText = "Nhập tên để tìm kiếm";
-            guna2TextBox1.SelectedText = "";
-            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            guna2TextBox1.Size = new Size(880, 36);
-            guna2TextBox1.TabIndex = 65;
-            // 
-            // panel2
-            // 
-            panel2.Controls.Add(dgvPhanQuyen);
-            panel2.Dock = DockStyle.Fill;
-            panel2.Location = new Point(0, 62);
-            panel2.Margin = new Padding(5, 3, 5, 3);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(1183, 759);
-            panel2.TabIndex = 65;
-            // 
             // iconButton2
             // 
             iconButton2.BackColor = Color.FromArgb(33, 31, 48);
@@ -257,6 +227,36 @@
             iconButton1.TabIndex = 66;
             iconButton1.UseVisualStyleBackColor = false;
             // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.CustomizableEdges = customizableEdges1;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(0, 192, 192);
+            guna2TextBox1.Location = new Point(12, 12);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PasswordChar = '\0';
+            guna2TextBox1.PlaceholderText = "Nhập tên để tìm kiếm";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2TextBox1.Size = new Size(880, 36);
+            guna2TextBox1.TabIndex = 65;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(dgvPhanQuyen);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 62);
+            panel2.Margin = new Padding(5, 3, 5, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1183, 759);
+            panel2.TabIndex = 65;
+            // 
             // PhanQuyenForm
             // 
             AutoScaleDimensions = new SizeF(11F, 25F);
@@ -267,7 +267,7 @@
             Controls.Add(panel1);
             Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(5, 5, 5, 5);
+            Margin = new Padding(5);
             Name = "PhanQuyenForm";
             Text = "PhanQuyenForm";
             ((System.ComponentModel.ISupportInitialize)dgvPhanQuyen).EndInit();
