f81f26902efc11c1bf6e701ad460860e73f0f31e
elio_0406 <stsupermarik@gmail.com>
2023-11-24 02:23:45 +0700

    Tải dữ liệu lên form bán hàng

---
+++ GUI/DonDatHangControl.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace GUI
+{
+    partial class DonDatHangControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            guna2NumericUpDown1 = new Guna.UI2.WinForms.Guna2NumericUpDown();
+            iconButton8 = new FontAwesome.Sharp.IconButton();
+            label2 = new Label();
+            label1 = new Label();
+            guna2ShadowPanel1.SuspendLayout();
+            guna2CustomGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2NumericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2ShadowPanel1
+            // 
+            guna2ShadowPanel1.BackColor = Color.Transparent;
+            guna2ShadowPanel1.Controls.Add(guna2CustomGradientPanel1);
+            guna2ShadowPanel1.Dock = DockStyle.Fill;
+            guna2ShadowPanel1.FillColor = Color.White;
+            guna2ShadowPanel1.Location = new Point(0, 0);
+            guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            guna2ShadowPanel1.Radius = 15;
+            guna2ShadowPanel1.ShadowColor = Color.Black;
+            guna2ShadowPanel1.Size = new Size(581, 53);
+            guna2ShadowPanel1.TabIndex = 0;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            guna2CustomGradientPanel1.BorderRadius = 5;
+            guna2CustomGradientPanel1.Controls.Add(guna2NumericUpDown1);
+            guna2CustomGradientPanel1.Controls.Add(iconButton8);
+            guna2CustomGradientPanel1.Controls.Add(label2);
+            guna2CustomGradientPanel1.Controls.Add(label1);
+            guna2CustomGradientPanel1.CustomizableEdges = customizableEdges3;
+            guna2CustomGradientPanel1.Dock = DockStyle.Fill;
+            guna2CustomGradientPanel1.Location = new Point(0, 0);
+            guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            guna2CustomGradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2CustomGradientPanel1.Size = new Size(581, 53);
+            guna2CustomGradientPanel1.TabIndex = 1;
+            // 
+            // guna2NumericUpDown1
+            // 
+            guna2NumericUpDown1.BackColor = Color.Transparent;
+            guna2NumericUpDown1.BorderRadius = 5;
+            guna2NumericUpDown1.CustomizableEdges = customizableEdges1;
+            guna2NumericUpDown1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            guna2NumericUpDown1.Location = new Point(254, 9);
+            guna2NumericUpDown1.Name = "guna2NumericUpDown1";
+            guna2NumericUpDown1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2NumericUpDown1.Size = new Size(72, 34);
+            guna2NumericUpDown1.TabIndex = 13;
+            guna2NumericUpDown1.UpDownButtonFillColor = Color.FromArgb(0, 50, 90);
+            guna2NumericUpDown1.UpDownButtonForeColor = Color.White;
+            // 
+            // iconButton8
+            // 
+            iconButton8.BackColor = Color.White;
+            iconButton8.FlatAppearance.BorderSize = 0;
+            iconButton8.FlatAppearance.MouseOverBackColor = Color.MistyRose;
+            iconButton8.FlatStyle = FlatStyle.Flat;
+            iconButton8.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            iconButton8.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            iconButton8.IconColor = Color.IndianRed;
+            iconButton8.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            iconButton8.IconSize = 20;
+            iconButton8.Location = new Point(546, 11);
+            iconButton8.Name = "iconButton8";
+            iconButton8.Padding = new Padding(0, 3, 0, 0);
+            iconButton8.Size = new Size(30, 30);
+            iconButton8.TabIndex = 12;
+            iconButton8.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(0, 50, 90);
+            label2.Location = new Point(354, 11);
+            label2.Name = "label2";
+            label2.Size = new Size(173, 30);
+            label2.TabIndex = 8;
+            label2.Text = "10.000.000 VNĐ";
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(236, 53);
+            label1.TabIndex = 7;
+            label1.Text = "Sữa tắm bảo vệ khỏi vi khuẩn lifebuoy chăm sóc da 980ml";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // DonDatHangControl
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Transparent;
+            Controls.Add(guna2ShadowPanel1);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "DonDatHangControl";
+            Size = new Size(581, 53);
+            guna2ShadowPanel1.ResumeLayout(false);
+            guna2CustomGradientPanel1.ResumeLayout(false);
+            guna2CustomGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2NumericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+        private Guna.UI2.WinForms.Guna2NumericUpDown guna2NumericUpDown1;
+        private FontAwesome.Sharp.IconButton iconButton8;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ GUI/SanPhamControl.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GUI
+{
+    partial class SanPhamControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(components);
+            guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            guna2PictureBox1.CustomizableEdges = customizableEdges1;
+            guna2PictureBox1.Image = Properties.Resources._004_lock;
+            guna2PictureBox1.ImageRotate = 0F;
+            guna2PictureBox1.Location = new Point(3, 3);
+            guna2PictureBox1.Name = "guna2PictureBox1";
+            guna2PictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2PictureBox1.Size = new Size(80, 80);
+            guna2PictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2PictureBox1.TabIndex = 0;
+            guna2PictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.FromArgb(223, 243, 252);
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(89, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 80);
+            label1.TabIndex = 1;
+            label1.Text = "cà phê chả trà sữa bún đá ";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.FromArgb(223, 243, 252);
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(0, 50, 90);
+            label2.Location = new Point(3, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(177, 25);
+            label2.TabIndex = 2;
+            label2.Text = "1.000.000 VNĐ";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.FromArgb(0, 50, 90);
+            iconButton1.Dock = DockStyle.Bottom;
+            iconButton1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            iconButton1.ForeColor = Color.White;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton1.IconColor = Color.Black;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(0, 114);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(183, 31);
+            iconButton1.TabIndex = 3;
+            iconButton1.Text = "Chọn";
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // SanPhamControl
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(223, 243, 252);
+            Controls.Add(iconButton1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(guna2PictureBox1);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5);
+            Name = "SanPhamControl";
+            Size = new Size(183, 145);
+            ((System.ComponentModel.ISupportInitialize)guna2PictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Label label1;
+        private Label label2;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
